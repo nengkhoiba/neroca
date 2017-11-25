0d62f125e779bf90ed71615d0b15379de3e4a522
WilliamThoudam <32706232+WilliamThoudam@users.noreply.github.com>
2017-11-25 12:52:12 +0530

    newnewnew

--- application/views/home.php
+++ application/views/home.php
@@ -1436,9 +1436,9 @@ To nurture sporting talents in general and Football in particular</p>
       <div class="footer-top clearfix">
         <div class="widget col-lg-3 col-md-3 col-sm-6">
           <h5 class="widget-title">About FC</h5>
-          <p>Lorem ipsum dolor sit amet consete adipisicing elit. Proin nibh augue.</p>
+          <p>Having long desire of the people of the northern Imphal side particularly along the Imphal-Dimapur National Highway No.39 (Now, NH2), the North Eastern Re-Organising Cultural Association (NEROCA), Sangakpham Bazar, Imphal, Manipur was established in 1965 with a good hope to enhance quality of youths especially in the field of cultural activities and sports arena. <a href="<?php echo base_url();?>home/about">Read more ..</a> </p>
           <ul class="contact-info custom-list">
-            <li><i class="fa fa-map-marker"></i><span>Lorem Ipsum</span></li>
+            <li><i class="fa fa-map-marker"></i><span> Sangakpham Rd, Chingmeirong, Heingang, Manipur 795010</span></li>
             <li><i class="fa fa-phone"></i><span>+1 (123) 456-7890</span></li>
             <li><i class="fa fa-envelope"></i><span><a href="mailto:example@example.com">example@example.com</a></span></li>
           </ul>
@@ -1446,52 +1446,108 @@ To nurture sporting talents in general and Football in particular</p>
         <div class="widget col-lg-3 col-md-3 col-sm-6">
           <h5 class="widget-title">Information</h5>
           <ul class="custom-list">
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
+            <li><a href="http://i-league.org/">Hero I-League</a></li>
+            
           </ul>
         </div>
         <div class="widget col-lg-3 col-md-3 col-sm-6">
-          <h5 class="widget-title">Support</h5>
+          <h5 class="widget-title">Site Links</h5>
           <ul class="custom-list">
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
-            <li><a href="#">Lorem Ipsum</a></li>
+            <li><a href="<?php echo base_url();?>">Home</a></li>
+            <li><a href="<?php echo base_url();?>home/about">About</a></li>
+            <li><a href="<?php echo base_url();?>home/achievement">Achievement</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">FAQ</a></li>
+           
           </ul>
         </div>
         <div class="widget col-lg-3 col-md-3 col-sm-6">
           <h5 class="widget-title">FC Newsletter</h5>
           <p>Join Neroca FC Mailing list and receive the latest news of Neroca FC ! Just enter your email address here and you are all set.</p>
           <form action="#" class="newsletter default-form">
-            <input type="text" placeholder="@me@example.com">
+            <input type="text" placeholder="you@example.com">
             <button class="button"><span>Register</span><i class="fa fa-arrow-circle-right"></i></button>
           </form>
         </div>
       </div>
       <!-- End Footer-Top -->
 
-      <!-- Start Sposnors -->
+ <div class="container">
+      
+      <!-- Start Ticket -->
+      <div class="ticket col-lg-4 col-md-12 col-sm-12" style="font-size: 13px; text-align: justify;">
+          <!-- Start Sposnors -->
       <div class="sponsors clearfix">
         <h5 class="text-center">Sponsors</h5>
-     
-        <div class="sponsor-logo text-center col-lg-12 col-md-12 col-sm-12">
-          <img src="<?php echo base_url();?>nero/img/partner5.png" alt="">
+        <div class="sponsor-logo spon text-center col-lg-2 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner1.png" alt="">
+        </div>
+        <div class="sponsor-logo spon text-center col-lg-2 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner2.png" alt="">
+        </div>
+        <div class="sponsor-logo spon text-center col-lg-2 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner3.png" alt="">
+        </div>
+        <div class="sponsor-logo spon text-center col-lg-2 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner4.png" alt="">
+        </div>
+        <div class="sponsor-logo spon text-center col-lg-2 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner5.png" alt="">
         </div>
        
       </div>
       <!-- End Sponsors -->
+       
+      </div>
+      <!-- End Ticket -->
+
+      
+
+      <!-- Start Table -->
+      <div class="table-info col-lg-4 col-md-12 col-sm-12">
+        
+        <!-- Start Sposnors -->
+      <div class="sponsors clearfix">
+        <h5 class="text-center">Events Partner</h5>
+        
+        <div class="sponsor-logo text-center col-lg-12 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner6.png" alt="">
+        </div>
+       
+      </div>
+      <!-- End Sponsors -->
+    </div>
+
+
+ 
+     <div class="ticket col-lg-4 col-md-12 col-sm-12">
+       
+         <!-- Start Sposnors -->
+      <div class="sponsors clearfix">
+        <h5 class="text-center">IT Partner</h5>
+        
+        <div class="sponsor-logo text-center col-lg-12 col-md-2 col-sm-4">
+          <img src="<?php echo base_url();?>nero/img/sponser/partner7.png" alt="">
+        </div>
+       
+      </div>
+      <!-- End Sponsors -->
+       
+      </div>
+
+
+
+    
+
+     
+
+
 
     </div>
 
     <!-- Start Copyrights -->
     <div class="copyrights clearfix text-center">
-      <p class="col-lg-12">© Copyright 2017. All Rights Reserved, powered by MOBIMP</p>
+      <p class="col-lg-12"><span class="pull-left"> © Copyright 2017. All Rights Reserved,</span><span class="pull-right"> Website Developed & Maintained By <a href="http://www.mobimp.com/"> MOBIMP Services Pvt. Ltd.</a></span></p>
     </div>
     <!-- End Copyrights -->
 
