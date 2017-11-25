d14a51809bde8b08f88265339bec4c08c7b15e7c
riyajuddin <32087292+riyajuddin@users.noreply.github.com>
2017-11-25 10:11:30 +0530

    neroca

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'mysql';
+$db['default']['password'] = '';
 $db['default']['database'] = 'I_league';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
--- application/views/getGate.php
+++ application/views/getGate.php
@@ -4,9 +4,11 @@ $id=$_GET['q'];
 $sql="SELECT ID, code, discription, capacity ,(SELECT COUNT(*) FROM `Seats` WHERE gate_id=GD.ID AND match_id=$id ) AS currentSeat FROM `Gate` GD WHERE ID!=1";
 $query = $this->db->query($sql);
 if($query){
+echo "<h3>Select Gate </h3>";
 	while($result=mysql_fetch_array($query->result_id))
 	{?>
-<li style="cursor:pointer" id="gd<?php echo $result['ID']?>" onclick="_Select_gate('<?php echo $result['ID']?>','<?php echo $result['capacity']?>','<?php echo $result['currentSeat']?>')" class="list-group-item" id="li_<?php echo $result['ID']?>"><a ><?php echo $result['code']?> <span><?php echo $result['currentSeat']?>/<?php echo $result['capacity']?><span></span></a></li>
+	
+<li style="cursor:pointer" id="gd<?php echo $result['ID']?>" onclick="_Select_gate('<?php echo $result['ID']?>','<?php echo $result['capacity']?>','<?php echo $result['currentSeat']?>')" class="list-group-item" id="li_<?php echo $result['ID']?>"><a ><?php echo "Gate ".$result['code']?> <span><?php echo " &nbsp;".$result['currentSeat']?>/<?php echo $result['capacity']?><span></span></a></li>
 <?php }
 }?>
 </ul>
